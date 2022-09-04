62531dc91a9903d1f6f3503a7742320fd46cacce
jtkyber <43868043+jtkyber@users.noreply.github.com>
2022-09-04 18:10:21 -0400

    'fix'

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import connectMongo from '../../utils/connectDB'
 import User from '../../models/userModel'
